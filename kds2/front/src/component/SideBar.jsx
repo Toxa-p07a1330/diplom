ba9e3666271d00c2341a629fea0a4e283d59b1e9
p07a1330 <p07a1330@yandex.ru>
2021-11-03 22:13:58 +0300

    with extra logout btn for mobile

--- kds2/front/src/component/SideBar.jsx
+++ kds2/front/src/component/SideBar.jsx
@@ -19,6 +19,9 @@ import { connect } from 'react-redux';
 import { userActions } from '../rdx/rdx';
 import { Nav, NavItem, NavLink } from 'reactstrap';
 import {faFileArchive} from "@fortawesome/fontawesome-free-regular";
+import MobileOnly from "./MobileOnly";
+import DesktopOnly from "./DesktopOnly";
+import LogoutBtn from "./LogoutBtn";
 
 class SideBar extends React.Component {
 
@@ -34,51 +37,59 @@ class SideBar extends React.Component {
 
   render() {
       return (
-          <Nav vertical className="fixed-left" id="sidebar">
-            <NavItem>
-              <NavLink tag={Link} to="/groups/"><FontAwesomeIcon icon={faListAlt} fixedWidth/>{' '}Groups</NavLink>
-            </NavItem>
-            <NavItem>
-                <NavLink tag={Link} to="/terminals/"><FontAwesomeIcon icon={faMobileAlt} fixedWidth/>{' '}Terminals</NavLink>
-            </NavItem>
-          <NavItem>
-              <NavLink tag={Link} to="/merchants/"><FontAwesomeIcon icon={faStore} fixedWidth/>{' '}Merchants</NavLink>
-          </NavItem>
+          <>
+              <Nav vertical className="fixed-left" id="sidebar">
+                <NavItem>
+                  <NavLink tag={Link} to="/groups/"><FontAwesomeIcon icon={faListAlt} fixedWidth/>{' '}Groups</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink tag={Link} to="/terminals/"><FontAwesomeIcon icon={faMobileAlt} fixedWidth/>{' '}Terminals</NavLink>
+                </NavItem>
               <NavItem>
-                  <NavLink tag={Link} to="/acquirers/"><FontAwesomeIcon icon={faCreditCard} fixedWidth/>{' '}Acquirers</NavLink>
+                  <NavLink tag={Link} to="/merchants/"><FontAwesomeIcon icon={faStore} fixedWidth/>{' '}Merchants</NavLink>
               </NavItem>
-            <NavItem>
-                <NavLink tag={Link} to="/conftemplates/"><FontAwesomeIcon icon={faFileCode} fixedWidth/>{' '}Config templates</NavLink>
-            </NavItem>
-            <NavItem>
-                <NavLink tag={Link} to="/confpacks/"><FontAwesomeIcon icon={faDatabase} fixedWidth/>{' '}Config packages</NavLink>
-            </NavItem>
-              <NavItem>
-                  <NavLink tag={Link} to="/applications/"><FontAwesomeIcon icon={faBug} fixedWidth/>{' '}Applications</NavLink>
-              </NavItem>
-          <NavItem>
-              <NavLink tag={Link} to="/activators/"><FontAwesomeIcon icon={faMagic} fixedWidth/>{' '}Activation</NavLink>
-          </NavItem>
-              <NavItem>
-                  <NavLink tag={Link} to="/keys/"><FontAwesomeIcon icon={faKey} fixedWidth/>{' '}Keys</NavLink>
-              </NavItem>
-              <NavItem>
-                  <NavLink tag={Link} to="/keyloaders/"><FontAwesomeIcon icon={faHdd} fixedWidth/>{' '}Key Loaders</NavLink>
-          </NavItem>
-              { this.props.loggedIn && this.props.user.admin &&
-                  <>
-              <NavItem>
-                  <NavLink tag={Link} to="/users/"><FontAwesomeIcon icon={faUsers} fixedWidth/>{' '}Users</NavLink>
-              </NavItem>
-              <NavItem>
-                  <NavLink tag={Link} to="/logs/"><FontAwesomeIcon icon={faFileArchive} fixedWidth/>{' '}Logs</NavLink>
+                  <NavItem>
+                      <NavLink tag={Link} to="/acquirers/"><FontAwesomeIcon icon={faCreditCard} fixedWidth/>{' '}Acquirers</NavLink>
                   </NavItem>
-              </>
-              }
-            <NavItem>
-                <NavLink tag={Link} to={"/accounts/" + this.props.user.id}><FontAwesomeIcon icon={faUser} fixedWidth/>{' '}My account</NavLink>
-            </NavItem>
-          </Nav>
+                <NavItem>
+                    <NavLink tag={Link} to="/conftemplates/"><FontAwesomeIcon icon={faFileCode} fixedWidth/>{' '}Config templates</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink tag={Link} to="/confpacks/"><FontAwesomeIcon icon={faDatabase} fixedWidth/>{' '}Config packages</NavLink>
+                </NavItem>
+                  <NavItem>
+                      <NavLink tag={Link} to="/applications/"><FontAwesomeIcon icon={faBug} fixedWidth/>{' '}Applications</NavLink>
+                  </NavItem>
+              <NavItem>
+                  <NavLink tag={Link} to="/activators/"><FontAwesomeIcon icon={faMagic} fixedWidth/>{' '}Activation</NavLink>
+              </NavItem>
+                  <NavItem>
+                      <NavLink tag={Link} to="/keys/"><FontAwesomeIcon icon={faKey} fixedWidth/>{' '}Keys</NavLink>
+                  </NavItem>
+                  <NavItem>
+                      <NavLink tag={Link} to="/keyloaders/"><FontAwesomeIcon icon={faHdd} fixedWidth/>{' '}Key Loaders</NavLink>
+              </NavItem>
+                  { this.props.loggedIn && this.props.user.admin &&
+                      <>
+                  <NavItem>
+                      <NavLink tag={Link} to="/users/"><FontAwesomeIcon icon={faUsers} fixedWidth/>{' '}Users</NavLink>
+                  </NavItem>
+                  <NavItem>
+                      <NavLink tag={Link} to="/logs/"><FontAwesomeIcon icon={faFileArchive} fixedWidth/>{' '}Logs</NavLink>
+                      </NavItem>
+                  </>
+                  }
+                  <NavItem>
+                      <NavLink tag={Link} to={"/accounts/" + this.props.user.id}><FontAwesomeIcon icon={faUser} fixedWidth/>{' '}My account</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <MobileOnly isOpen={1}>
+                        <LogoutBtn/>
+                    </MobileOnly>
+                  </NavItem>
+              </Nav>
+
+          </>
       );
   }
 }
