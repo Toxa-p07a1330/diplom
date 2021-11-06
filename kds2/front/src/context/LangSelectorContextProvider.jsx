c4aea07ec66f59deb9d245e0e23908f3ec5ead61
p07a1330 <p07a1330@yandex.ru>
2021-11-06 18:46:59 +0300

    with lang selector auto

--- kds2/front/src/context/LangSelectorContextProvider.jsx
+++ kds2/front/src/context/LangSelectorContextProvider.jsx
@@ -2,8 +2,13 @@ import React, { useState } from 'react';
 
 const LangSelectorContext = React.createContext();
 let LangSelectorContextProvider = props => {
+
+    let language = window.navigator ? (window.navigator.language ||
+        window.navigator.systemLanguage ||
+        window.navigator.userLanguage): "ru"
+    language = language.substr(0, 2).toLowerCase();
     const [state, setState] = useState({
-        lang: "ru"
+        lang: language
     });
     return (
         <LangSelectorContext.Provider value={{ data: state, setData: setState }}>
