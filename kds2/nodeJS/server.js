2819198a2b2919c082952f2136d5fe972cdd57ce
p07a1330 <p07a1330@yandex.ru>
2021-11-07 21:36:17 +0300

    with logging backend added

--- kds2/nodeJS/server.js
+++ kds2/nodeJS/server.js
@@ -1,12 +1,14 @@
 const mysql = require("mysql2");
+const express = require("express");
+const app = express();
 
-const connection = mysql.createConnection({
+const connection = mysql.createConnection({     //настройки покключения
     host: "localhost",
     user: "root",
     database: "kds",
     password: "swzYW92W"
 });
-connection.connect(function(err){
+connection.connect(function(err){       //подключение к базе данных
     if (err) {
         return console.error("Ошибка: " + err.message);
     }
@@ -15,8 +17,39 @@ connection.connect(function(err){
     }
 });
 
-connection.execute("SELECT * FROM users",
-    function(err, results, fields) {
-        console.log(results); // собственно данные
-    });
-connection.end();
+
+
+app.listen(8081);   //настройки сокета
+app.use(express.json())
+
+
+app.post("/api/log", function(request, response){
+    console.log(request.body)
+    try {
+        writeNewLogToDatabase(request.body.date, request.body.level, request.body.user, request.body.message, response)
+    }
+    catch (e){
+        console.log(e);
+        response.send(JSON.stringify(e));
+    }
+});
+
+
+let writeNewLogToDatabase = async (date, level, user, message, responce)=>{             //запись нового лога в БД
+    let sqlStr = "INSERT INTO logs (date, level, user, message) values" +
+        "('"+date+"', '"+level+"', '"+user+"', '"+message+"')";
+    console.log(sqlStr)
+    connection.execute(sqlStr,
+        function(err, results, fields) {
+            if (err){
+                responce.send(JSON.stringify(err))
+            }
+            else {
+                responce.send(results)
+            }
+        });
+ 
+}
+
+
+
