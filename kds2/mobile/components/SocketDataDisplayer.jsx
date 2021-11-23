23e44dac3dd3cf18cc979ad553c5211fae1127f4
p07a1330 <p07a1330@yandex.ru>
2021-11-24 00:03:32 +0300

    implementer server side trigger api

--- kds2/mobile/components/SocketDataDisplayer.jsx
+++ kds2/mobile/components/SocketDataDisplayer.jsx
@@ -1,6 +1,5 @@
 import {Button, Text, View} from "react-native";
 import React, {useEffect, useState} from "react";
-
 const wayToSocket = "ws://192.168.0.12:8082"
 
 import io from "socket.io-client";
@@ -8,13 +7,16 @@ import {TextInput} from "react-native";
 
 export default function SocketDataDisplayer() {
     let [state, setState] = useState("start")
-    const TID = 863145;
+    const TID = 123456; //Math.floor(Math.random()*100000);       //имитация ID терминала
 
     let socket = io(wayToSocket);
     socket.on("message", msg => {
         console.log(msg)
         setState(msg);
     });
+    socket.on("update", msg=>{
+        alert("update");
+    })
 
     useEffect(()=>{
         socket.emit("init", JSON.stringify({
--- kds2/nodeJS/socketServer/serverSocket.js
+++ kds2/nodeJS/socketServer/serverSocket.js
@@ -3,10 +3,12 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
+const url = require('url');
+const querystring = require('querystring');
 
 const port = 8082
 const io = new Server(server);
-const TID_connections = []
+let TID_connections = []
 
 let find_TID = (TID)=>{
     let rez = TID_connections.filter((value)=>{
@@ -24,17 +26,44 @@ io.on("connection", socket => {
         let TID = JSON.parse(data).TID;
         if (!find_TID(TID)){
             TID_connections.push({
-                connection_id: id,
-                TID: TID
+                socket: socket,
+                TID: TID,
+                id: socket.id
             });
-            console.log(TID_connections);
         }
+    });
+
+    socket.on("disconnect", (reason) => {
+        console.log("Device disconnected");
+        TID_connections = TID_connections.filter((value)=>{
+            return value.id !== socket.id;
+        })
     });
 });
 
+let getQueryParams = (search) => {
+    let params = {};
+    let key_value_array = search.substring(1).split('&');
+    key_value_array.map((value, index, array) => {
+        let key = value.split('=')[0];
+        let val = value.split('=')[1];
+        params[key] = val;
+    });
+    return params;
+};
+
 
 const expressPort = 8083;
-app.get('/', (req, res) => {
+app.get('*/sendCommand', (req, res) => {
+    let params = url.parse(req.url);
+    let parsedUrl = url.parse(req.url);
+    let parsedQs = querystring.parse(parsedUrl.query);
+
+    console.log(parsedQs)
+
+    let socket = TID_connections.filter((value)=>{return value.TID === parsedQs.TID})[0].socket;
+    socket.emit("update", "some msg");
+
     res.send('Hello World!')
 })
 app.listen(expressPort)
